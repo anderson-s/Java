fa9187561bdc882938dd729df0b68704a161a03b
anderson-s <42554525+anderson-s@users.noreply.github.com>
2018-10-25 12:42:37 -0300

    Create sucessor.java

---
+++ sucessor.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class Anderson {
+
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        int num = tcl.nextInt();
+        
+        System.out.println(num + 1);
+        
+    }
+    
+}
